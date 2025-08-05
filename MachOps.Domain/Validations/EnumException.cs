8f0f88710124c477aa27a35b902cba246cb0a75e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-05 19:07:49 -0300

    fix: altera o tipo do parametro type para inteiro

--- MachOps.Domain/Validations/EnumException.cs
+++ MachOps.Domain/Validations/EnumException.cs
@@ -4,7 +4,7 @@ public sealed class EnumException : Exception
 {
     public EnumException(string message) : base(message) { }
 
-    public static void ThrowIfNotDefined<T>(T type, string message) where T : Enum
+    public static void ThrowIfNotDefined<T>(int type, string message) where T : Enum
     {
         if (!Enum.IsDefined(typeof(T), type))
             throw new EnumException(message);
