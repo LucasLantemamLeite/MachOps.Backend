d4c0f72ef6655c57acd86e78e17f1c005bd6de93
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-05 21:41:57 -0300

    feat: cria os arquivos padrões de migration

---
+++ MachOps.Migrations/Migrations/20250806003715_InitialDeploy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MachOps.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "Int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "Nvarchar(30)", nullable: false),
+                    MachType = table.Column<int>(type: "Int", nullable: false),
+                    Status = table.Column<int>(type: "Int", nullable: false),
+                    Location = table.Column<string>(type: "Nvarchar(20)", nullable: true),
+                    CreateAt = table.Column<DateTime>(type: "Datetime", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    Description = table.Column<string>(type: "Nvarchar(100)", nullable: true),
+                    MaintenceStartDate = table.Column<DateTime>(type: "DateTime2", nullable: true),
+                    ExpectedReturnDate = table.Column<DateTime>(type: "DateTime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_Name_Machines",
+                table: "Machines",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Machines");
+        }
+    }
+}
