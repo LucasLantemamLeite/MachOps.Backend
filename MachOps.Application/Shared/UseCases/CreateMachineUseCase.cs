a03a353e0efe60050d310f74133963ea9a34e233
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-06 12:37:55 -0300

    refactor: adiciona a herança ao BaseUseCase para facilitar as injeções de Query e Repository

--- MachOps.Application/Shared/UseCases/CreateMachineUseCase.cs
+++ MachOps.Application/Shared/UseCases/CreateMachineUseCase.cs
@@ -1,38 +1,53 @@
 using MachOps.Application.Interfaces.Queries;
 using MachOps.Application.Interfaces.Repositories;
 using MachOps.Application.Shared.Commands;
-using MachOps.Application.Shared.Result;
+using MachOps.Application.Shared.ResultCases;
+using MachOps.Application.Shared.UseCases.Base;
 using MachOps.Domain.Entities;
 using MachOps.Domain.ValueObjects.Validations;
 
 namespace MachOps.Application.Shared.UseCases;
 
-public sealed class CreateMachineUseCase
+public sealed class CreateMachineUseCase : BaseUseCase<IMachineQuery, IMachineRepository>
 {
-    private readonly IMachineRepository _repository;
-    private readonly IMachineQuery _query;
-
-    public CreateMachineUseCase(IMachineRepository repository, IMachineQuery query)
-    {
-        _repository = repository;
-        _query = query;
-    }
+    public CreateMachineUseCase(IMachineQuery query, IMachineRepository repository) : base(query, repository) { }
 
     public async Task<Result<MachineEntity>> ExecuteAsync(CreateMachineCommand command)
     {
         try
         {
+            var existingMachine = await Query.GetByNameAsync(command.Name);
 
+            if (existingMachine is not null)
+                return Result<MachineEntity>.Fail("Name já está em uso.");
+
+            var machine = new MachineEntity
+            (
+                command.Name,
+                command.MachType,
+                command.Location,
+                command.Status,
+                command.Description,
+                command.MaintenceStartDate,
+                command.ExpectedReturnDate
+            );
+
+            var row = await Repository.CreateAsync(machine);
+
+            if (row == 0)
+                return Result<MachineEntity>.Fail("Erro interno no servidor. Falha ao criar nova Machine");
+
+            return Result<MachineEntity>.Ok("Machine criado com sucesso.", machine);
         }
 
         catch (Exception ex) when (ex is DomainException or EnumException)
         {
-
+            return Result<MachineEntity>.Fail($"{ex.Message}");
         }
 
         catch
         {
-
+            return Result<MachineEntity>.Fail("Erro interno do servidor. Tente novamento mais tarde.");
         }
     }
 }
