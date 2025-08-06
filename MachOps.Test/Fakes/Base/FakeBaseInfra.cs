6b59eb93c7db43e31c703ce0d79c97313da8498c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-06 17:57:49 -0300

    feat: cria a base abstrata para simular um banco em formato de lista

---
+++ MachOps.Test/Fakes/Base/FakeBaseInfra.cs
@@ -0,0 +1,58 @@
+using MachOps.Domain.Entities;
+
+namespace MachOps.Test.Fakes.Base;
+
+public abstract class FakeBaseDb
+{
+    protected IEnumerable<MachineEntity> machines = new List<MachineEntity>
+    {
+        new(
+            id: 1,
+            name: "Empilhadeira",
+            machType: 1,
+            status: 2,
+            location: "Galpão A",
+            create: new DateTime(2025, 1, 15),
+            update: new DateTime(2025, 6, 10),
+            description: "",
+            maintenceStart: new DateTime(2025, 8, 1),
+            expectedReturn: new DateTime(2025, 8, 5)
+        ),
+        new(
+            id: 2,
+            name: "Guindaste",
+            machType: 6,
+            status: 3,
+            location: "Doca 3",
+            create: new DateTime(2024, 12, 1),
+            update: new DateTime(2025, 7, 25),
+            description: "Em manutenção crítica.",
+            maintenceStart: new DateTime(2025, 7, 20),
+            expectedReturn: new DateTime(2025, 8, 10)
+        ),
+        new(
+            id: 3,
+            name: "Bulldozer",
+            machType: 2,
+            status: 1,
+            location: "Pátio de Manobras",
+            create: new DateTime(2025, 3, 3),
+            update: new DateTime(2025, 7, 30),
+            description: "Utilizada para limpeza geral.",
+            maintenceStart: null,
+            expectedReturn: null
+        ),
+        new(
+            id: 4,
+            name: "Trator",
+            machType: 5,
+            status: 1,
+            location: "Galpão B",
+            create: new DateTime(2025, 2, 5),
+            update: new DateTime(2025, 6, 20),
+            description: "Auxiliando no transporte de cargas.",
+            maintenceStart: new DateTime(2025, 7, 15),
+            expectedReturn: new DateTime(2025, 7, 18)
+        )
+    };
+}
