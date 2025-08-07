1e851fb8008a4608bf1d55f6ad2d85d81871d50b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-06 21:21:07 -0300

    refactor: altera o retorno dos métodos de IEnumerable para List

--- MachOps.Application/Interfaces/Queries/IMachineQuery.cs
+++ MachOps.Application/Interfaces/Queries/IMachineQuery.cs
@@ -4,12 +4,12 @@ namespace MachOps.Application.Interfaces.Queries;
 
 public interface IMachineQuery
 {
-    Task<IEnumerable<MachineEntity>> GetAllAsync();
+    Task<List<MachineEntity>> GetAllAsync();
     Task<MachineEntity?> GetByIdAsync(int id);
     Task<MachineEntity?> GetByNameAsync(string name);
-    Task<IEnumerable<MachineEntity>> GetByMachTypeAsync(int type);
-    Task<IEnumerable<MachineEntity>> GetByStatusAsync(int status);
-    Task<IEnumerable<MachineEntity>> GetByIntervalAsync(DateTime maintenanceStart, DateTime expectReturn);
-    Task<IEnumerable<MachineEntity>> GetByMaintenceStartDateAsync(DateTime maintenanceStart);
-    Task<IEnumerable<MachineEntity>> GetByExpectedReturnDateAsync(DateTime expectReturn);
+    Task<List<MachineEntity>> GetByMachTypeAsync(int type);
+    Task<List<MachineEntity>> GetByStatusAsync(int status);
+    Task<List<MachineEntity>> GetByIntervalAsync(DateTime maintenanceStart, DateTime expectReturn);
+    Task<List<MachineEntity>> GetByMaintenceStartDateAsync(DateTime maintenanceStart);
+    Task<List<MachineEntity>> GetByExpectedReturnDateAsync(DateTime expectReturn);
 }
