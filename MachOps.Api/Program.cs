642e78ca8047bae46fdb818860ea1c74e524438a
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 22:18:20 -0300

    feat: utiliza os extension method para configurar a Api

--- MachOps.Api/Program.cs
+++ MachOps.Api/Program.cs
@@ -1,5 +1,19 @@
+using MachOps.Api.Configurations.Injections;
+using MachOps.Api.Configurations.Build;
+using MachOps.Api.Configurations.App;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .RegisterHandlers()
+    .RegisterQueries()
+    .RegisterRepositories()
+    .RegisterServices(builder.Configuration);
+
+builder.RegisterConfig();
+
 var app = builder.Build();
+
+app.RegisterConfig();
 
 app.Run();
