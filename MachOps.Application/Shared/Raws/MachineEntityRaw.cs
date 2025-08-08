619bc8a433b16e1937519c055d19af15db941951
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 23:34:43 -0300

    feat: cria novo construtor para instanciar um EntityRaw e permite datas nulas

--- MachOps.Application/Shared/Raws/MachineEntityRaw.cs
+++ MachOps.Application/Shared/Raws/MachineEntityRaw.cs
@@ -10,6 +10,20 @@ public sealed class MachineEntityRaw
     public DateTime CreateAt { get; }
     public DateTime UpdateAt { get; }
     public string? Description { get; }
-    public DateTime MaintenceStartDate { get; }
-    public DateTime ExpectedReturnDate { get; }
+    public DateTime? MaintenceStartDate { get; }
+    public DateTime? ExpectedReturnDate { get; }
+
+    public MachineEntityRaw(int id, string name, int machType, int status, string? location, DateTime createAt, DateTime updateAt, string? description, DateTime? maintenceStartDate, DateTime? expectedReturnDate)
+    {
+        Id = id;
+        Name = name;
+        MachType = machType;
+        Status = status;
+        Location = location;
+        CreateAt = createAt;
+        UpdateAt = updateAt;
+        Description = description;
+        MaintenceStartDate = maintenceStartDate;
+        ExpectedReturnDate = expectedReturnDate;
+    }
 }
