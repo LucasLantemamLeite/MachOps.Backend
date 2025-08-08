c5230330831a1768fd61d6c6ff75c617fcf14b44
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 23:41:04 -0300

    feat: cria dois métodos para fazer o contrários dos anteriores
    Conversão usado no retorno da requisição para remover os 'Value' do json facilitando a visualização e o Frontend

--- MachOps.Application/Shared/Mappers/MachineEntityMapper.cs
+++ MachOps.Application/Shared/Mappers/MachineEntityMapper.cs
@@ -23,4 +23,23 @@ public static class MachineEntityMapper
     }
 
     public static List<MachineEntity> ToListMachineEntity(this IEnumerable<MachineEntityRaw> machineRaws) => machineRaws.Select(m => m.ToSingleMachineEntity()).ToList();
+
+    public static MachineEntityRaw ToSingleMachineEntityRaw(this MachineEntity machineEntity)
+    {
+        return new MachineEntityRaw
+        (
+            machineEntity.Id,
+            machineEntity.Name.Value,
+            (int)machineEntity.MachType.Value,
+            (int)machineEntity.Status.Value,
+            machineEntity.Location.Value,
+            machineEntity.CreateAt.Value,
+            machineEntity.UpdateAt.Value,
+            machineEntity.Description.Value,
+            machineEntity.MaintenceStartDate?.Value,
+            machineEntity.ExpectedReturnDate?.Value
+        );
+    }
+
+    public static List<MachineEntityRaw> ToListMachineEntityRaw(this List<MachineEntity> machineEntities) => machineEntities.Select(m => m.ToSingleMachineEntityRaw()).ToList();
 }
