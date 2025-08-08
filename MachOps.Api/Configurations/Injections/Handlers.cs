8eace1a3332eddb69eb2dac60a6d694545e13b1d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 21:44:41 -0300

    feat: cria o extension method para injetar os handler, por agora, vazio

---
+++ MachOps.Api/Configurations/Injections/Handlers.cs
@@ -0,0 +1,9 @@
+namespace MachOps.Api.Configurations.Injections;
+
+public static class HandlerInjection
+{
+    public static IServiceCollection RegisterHandlers(this IServiceCollection service)
+    {
+        return service;
+    }
+}
