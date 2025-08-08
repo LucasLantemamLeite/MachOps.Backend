1cdda4c20eeff0f285d1733f204817d92aff3939
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 21:46:42 -0300

    feat: remove o UseSqlServer e passa para a api, e cria o construtor para injetar o AppDbContext

--- MachOps.Migrations/Data/Context/AppDbContext.cs
+++ MachOps.Migrations/Data/Context/AppDbContext.cs
@@ -5,10 +5,9 @@ namespace MachOps.Migrations.Data.Context;
 
 public sealed class AppDbContext : DbContext
 {
-    public DbSet<MachineEntity> Machines { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-        optionsBuilder.UseSqlServer("server=localhost, 1433;database=MachOpsDb;User Id=sa;Password=Lucas1971!;TrustServerCertificate=True;");
+    public DbSet<MachineEntity> Machines { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 }
