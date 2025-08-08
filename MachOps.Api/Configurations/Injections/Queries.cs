2314db5e86fa77e9043f8888436ecbd1e39d9ed1
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 21:44:58 -0300

    feat: cria o extension method para injetar as queries

---
+++ MachOps.Api/Configurations/Injections/Queries.cs
@@ -0,0 +1,14 @@
+using MachOps.Application.Interfaces.Queries;
+using MachOps.Infrastructure.Implementations.Queries;
+
+namespace MachOps.Api.Configurations.Injections;
+
+public static class QueryInjection
+{
+    public static IServiceCollection RegisterQueries(this IServiceCollection service)
+    {
+        service.AddScoped<IMachineQuery, MachineQuery>();
+
+        return service;
+    }
+}
