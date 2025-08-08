fcc9fd7a2d091b6fe81eedf233e7a5c2d07ad54f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 23:28:52 -0300

    feat: adiciona a injeção do CreateMachineHandler

--- MachOps.Api/Configurations/Injections/Handlers.cs
+++ MachOps.Api/Configurations/Injections/Handlers.cs
@@ -1,9 +1,14 @@
+using MachOps.Api.Orchestrations.Handlers;
+using MachOps.Application.Interfaces.Handlers;
+
 namespace MachOps.Api.Configurations.Injections;
 
 public static class HandlerInjection
 {
     public static IServiceCollection RegisterHandlers(this IServiceCollection service)
     {
+        service.AddScoped<ICreateMachineHandler, CreateMachineHandler>();
+
         return service;
     }
 }
