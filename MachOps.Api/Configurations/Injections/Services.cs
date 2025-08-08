0c404ef4f024bbafabe1f73b84ef0f1d6024ca31
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-07 21:45:35 -0300

    feat: cria o extension method para injetar os services

---
+++ MachOps.Api/Configurations/Injections/Services.cs
@@ -0,0 +1,19 @@
+using System.Data;
+using MachOps.Migrations.Data.Context;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace MachOps.Api.Configurations.Injections;
+
+public static class ServiceInjetion
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection service, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        service.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+        service.AddScoped<IDbConnection>(sp => new SqlConnection(connectionString));
+
+        return service;
+    }
+}
