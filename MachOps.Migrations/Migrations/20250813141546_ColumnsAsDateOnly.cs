2ae03a3873a9afc93fdff0fc64fa67848f74f0f0
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-13 11:40:50 -0300

    refactor: cria o nome arquivo de migrations com os campos de date alterados

---
+++ MachOps.Migrations/Migrations/20250813141546_ColumnsAsDateOnly.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MachOps.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnsAsDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "MaintenanceStartDate",
+                table: "Machines",
+                type: "Date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DateTime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpectedReturnDate",
+                table: "Machines",
+                type: "Date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DateTime",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "MaintenanceStartDate",
+                table: "Machines",
+                type: "DateTime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "Date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpectedReturnDate",
+                table: "Machines",
+                type: "DateTime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "Date",
+                oldNullable: true);
+        }
+    }
+}
