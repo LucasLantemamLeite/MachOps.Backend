2a2864cc9b4ad57eb0ecf19bf2c1ea597966c3d4
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 15:14:34 -0300

    feat: adiciona a injeção das Queries e do Record

--- MachOps.Api/Configurations/Injections/Queries.cs
+++ MachOps.Api/Configurations/Injections/Queries.cs
@@ -1,4 +1,5 @@
 using MachOps.Application.Interfaces.Queries;
+using MachOps.Application.Shared.UseCases.Base.Utils;
 using MachOps.Infrastructure.Implementations.Queries;
 
 namespace MachOps.Api.Configurations.Injections;
@@ -7,7 +8,13 @@ public static class QueryInjection
 {
     public static IServiceCollection RegisterQueries(this IServiceCollection service)
     {
+        service.AddScoped<IUserAccountQuery, UserAccountQuery>();
         service.AddScoped<IMachineryQuery, MachineryQuery>();
+
+        service.AddScoped(sp => new Queries(
+            sp.GetRequiredService<IUserAccountQuery>(),
+            sp.GetRequiredService<IMachineryQuery>()
+        ));
 
         return service;
     }
