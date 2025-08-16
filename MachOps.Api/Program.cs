b998813983e33fe964deafca50f5d58f3414f88c
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 15:19:48 -0300

    feat: regista os UseCases no program

--- MachOps.Api/Program.cs
+++ MachOps.Api/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .RegisterQueries()
     .RegisterRepositories()
+    .RegisterUseCases()
     .RegisterHandlers()
     .RegisterServices(builder.Configuration);
 
