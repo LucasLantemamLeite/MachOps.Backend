b81963cee451d37702aacc47a4e43c454baeb701
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 11:04:35 -0300

    feat: adiciona a tabela de Users no context

--- MachOps.Migrations/Data/Context/AppDbContext.cs
+++ MachOps.Migrations/Data/Context/AppDbContext.cs
@@ -8,6 +8,7 @@ public sealed class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Machinery> Machines { get; set; }
+    public DbSet<UserAccount> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 }
