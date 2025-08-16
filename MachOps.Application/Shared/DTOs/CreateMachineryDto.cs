9c53f42d9719aa037579b0e031cf943d15f2bc6f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 21:11:36 -0300

    refactor: altera o nome do arquivo classe e propriedades

--- MachOps.Application/Shared/DTOs/CreateMachineryDto.cs
+++ MachOps.Application/Shared/DTOs/CreateMachineryDto.cs
@@ -2,25 +2,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MachOps.Application.Shared.DTOs;
 
-public sealed class CreateMachineDto
+public sealed class CreateMachineryDto
 {
     [Required(ErrorMessage = "O campo Name é obrigatório.")]
     [MaxLength(30, ErrorMessage = "O campo Name não pode ter mais de 30 caracteres.")]
-    public string Name { get; init; }
+    public string MachineryName { get; init; }
 
     [Required(ErrorMessage = "O campo MachType é obrigatório.")]
     [Range(1, 6, ErrorMessage = "MachType inválido.")]
-    public int MachType { get; init; }
+    public int MachineryType { get; init; }
 
     [Required(ErrorMessage = "O campo Status é obrigatório.")]
     [Range(1, 3, ErrorMessage = "Status inválido.")]
-    public int Status { get; init; }
+    public int MachineryStatus { get; init; }
 
     [MaxLength(20, ErrorMessage = "O campo de Location não pode ter menos de 20 caracteres.")]
-    public string? Location { get; init; }
+    public string? MachineryLocation { get; init; }
 
     [MaxLength(100, ErrorMessage = "O campo de Description deve ter menos de 100 caracteres.")]
-    public string? Description { get; init; }
+    public string? MachineryDescription { get; init; }
 
     [DataType(DataType.DateTime, ErrorMessage = "MaintenanceStartDate Inválido.")]
     public DateTime? MaintenanceStartDate { get; init; }
