c08f80e7dd390bd2a3921fd0b622740f5582cfba
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 15:35:17 -0300

    refactor: altera o nome do método

--- MachOps.Application/Interfaces/Repositories/IUserAccountRepository.cs
+++ MachOps.Application/Interfaces/Repositories/IUserAccountRepository.cs
@@ -5,6 +5,6 @@ namespace MachOps.Application.Interfaces.Repositories;
 public interface IUserAccountRepository
 {
     Task<int> CreateAsync(UserAccount user);
-    Task<int> DeleteAsync(UserAccount user);
+    Task<int> DisableAsync(UserAccount user);
     Task<int> UpdateAsync(UserAccount user);
 }
