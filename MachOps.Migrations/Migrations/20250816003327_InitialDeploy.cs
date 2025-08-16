b6d280536f779ef32ef00e1ad18fb753c5b2dc73
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 21:35:10 -0300

    feat: remove os migrations anteriores e adiciona a nova versão
    Por conta grande mudança na aplicação grande parte das migrations antigas não funcionaram, obrigando a deletar

--- MachOps.Migrations/Migrations/20250816003327_InitialDeploy.cs
+++ MachOps.Migrations/Migrations/20250816003327_InitialDeploy.cs
@@ -18,14 +18,14 @@ namespace MachOps.Migrations.Migrations
                     Id = table.Column<int>(type: "Int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "Nvarchar(30)", nullable: false),
-                    MachType = table.Column<int>(type: "Int", nullable: false),
+                    Type = table.Column<int>(type: "Int", nullable: false),
                     Status = table.Column<int>(type: "Int", nullable: false),
                     Location = table.Column<string>(type: "Nvarchar(20)", nullable: true),
                     CreateAt = table.Column<DateTime>(type: "Datetime", nullable: false),
                     UpdateAt = table.Column<DateTime>(type: "DateTime2", nullable: false),
                     Description = table.Column<string>(type: "Nvarchar(100)", nullable: true),
-                    MaintenanceStartDate = table.Column<DateTime>(type: "DateTime2", nullable: true),
-                    ExpectedReturnDate = table.Column<DateTime>(type: "DateTime", nullable: true)
+                    Start = table.Column<DateTime>(type: "Date", nullable: true),
+                    Return = table.Column<DateTime>(type: "Date", nullable: true)
                 },
                 constraints: table =>
                 {
