42e4aa52a7c7e69ea64a2ae753fb8b91fdb5a576
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 21:04:04 -0300

    delete: remove o handler que será recriado novamente

--- MachOps.Api/Configurations/Injections/Handlers.cs
+++ MachOps.Api/Configurations/Injections/Handlers.cs
@@ -1,13 +1,9 @@
-using MachOps.Api.Orchestrations.Handlers;
-using MachOps.Application.Interfaces.Handlers;
-
 namespace MachOps.Api.Configurations.Injections;
 
 public static class HandlerInjection
 {
     public static IServiceCollection RegisterHandlers(this IServiceCollection service)
     {
-        service.AddScoped<ICreateMachineHandler, CreateMachineHandler>();
 
         return service;
     }
