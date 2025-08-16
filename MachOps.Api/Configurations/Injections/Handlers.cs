56f810e2738fc8a6eff103427696e2f6aa71bdd8
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 15:14:23 -0300

    feat: adiciona a injeção do handler

--- MachOps.Api/Configurations/Injections/Handlers.cs
+++ MachOps.Api/Configurations/Injections/Handlers.cs
@@ -1,9 +1,13 @@
+using MachOps.Api.Orchestration.Handlers.Users;
+using MachOps.Application.Interfaces.Handlers.Users;
+
 namespace MachOps.Api.Configurations.Injections;
 
 public static class HandlerInjection
 {
     public static IServiceCollection RegisterHandlers(this IServiceCollection service)
     {
+        service.AddScoped<ICreateUserAccountHandler, CreateUserAccountHandler>();
 
         return service;
     }
