3ae12be5d75a14b61fbe3a918762ef6a6e0d75f5
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 22:41:33 -0300

    feat: cria a entidade UserAccount

--- MachOps.Domain/Entities/UserAccount.cs
+++ MachOps.Domain/Entities/UserAccount.cs
@@ -1,5 +1,37 @@
 using MachOps.Domain.Entities.Base;
+using MachOps.Domain.ValueObjects.Generics;
+using MachOps.Domain.ValueObjects.UserAccount;
 
 namespace MachOps.Domain.Entities;
 
-public sealed class UserAccount : Entity { }
+public sealed class UserAccount : Entity
+{
+    public Name Name { get; private set; }
+    public Email Email { get; private set; }
+    public Password Password { get; private set; }
+    public Phone Phone { get; private set; }
+    public CreatedAt CreatedAt { get; private set; }
+    public Active Active { get; private set; }
+    public Role Role { get; private set; }
+
+    public UserAccount(string name, string email, string password, string phone)
+    {
+        Name = new Name(name);
+        Email = new Email(email);
+        Password = new Password(password);
+        Phone = new Phone(phone);
+    }
+
+    public UserAccount(int id, string name, string email, string password, string phone, DateTime create, bool active, int role) : base(id)
+    {
+        Name = new Name(name);
+        Email = new Email(email);
+        Password = new Password(password);
+        Phone = new Phone(phone);
+        CreatedAt = new CreatedAt(create);
+        Active = new Active(active);
+        Role = new Role(role);
+    }
+
+    public UserAccount() { }
+}
