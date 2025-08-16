23bc1f236bb8033bb184593826da01988ac4b2a9
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 13:39:56 -0300

    delete: remove arquivo

--- MachOps.Application/Interfaces/Handlers/IGetAllMachineryHandler.cs
+++
@@ -1,9 +0,0 @@
-using MachOps.Application.Shared.ResultCases;
-using MachOps.Domain.Entities;
-
-namespace MachOps.Application.Interfaces.Handlers;
-
-public interface IGetAllMachineryHandler
-{
-    public Task<Result<List<Machinery>>> Handle();
-}
