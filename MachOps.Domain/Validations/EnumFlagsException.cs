ecd15c4e787ac1222f7e897929312e42317e6b04
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 10:42:56 -0300

    feat: melhora conversão para remove menssagem de null reference

--- MachOps.Domain/Validations/EnumFlagsException.cs
+++ MachOps.Domain/Validations/EnumFlagsException.cs
@@ -4,7 +4,7 @@ public sealed class EnumFlagsException(string message) : Exception(message)
 {
     public static void ThrowIfNotFlagDefined<TEnum>(int type, string message) where TEnum : struct, Enum
     {
-        var allDefinedValues = Enum.GetValues<TEnum>().Cast<int>().Aggregate((a, b) => a | b);
+        var allDefinedValues = Enum.GetValues<TEnum>().Select((e) => Convert.ToInt32(e)).Aggregate((a, b) => a | b);
 
         if ((type & ~allDefinedValues) != 0)
             throw new EnumFlagsException(message);
