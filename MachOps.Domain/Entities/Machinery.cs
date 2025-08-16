753cb0eaf7c446d442833de6786036c463a031e2
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 22:26:58 -0300

    refactor: altera classes e propriedades

--- MachOps.Domain/Entities/Machinery.cs
+++ MachOps.Domain/Entities/Machinery.cs
@@ -1,46 +1,47 @@
 using MachOps.Domain.Entities.Base;
+using MachOps.Domain.ValueObjects.Generics;
 using MachOps.Domain.ValueObjects.Machinery;
 
 namespace MachOps.Domain.Entities;
 
 public sealed class Machinery : Entity
 {
-    public MachineryName MachineryName { get; private set; }
-    public MachineryType MachineryType { get; private set; }
-    public MachineryStatus MachineryStatus { get; private set; }
-    public MachineryLocation MachineryLocation { get; private set; }
-    public MachineryCreatedAt MachineryCreatedAt { get; private set; }
-    public MachineryLastUpdatedAt MachineryLastUpdatedAt { get; private set; }
-    public MachineryDescription MachineryDescription { get; private set; }
-    public MaintenanceStartDate MaintenanceStartDate { get; private set; }
-    public ExpectedReturnDate ExpectedReturnDate { get; private set; }
+    public Name Name { get; private set; }
+    public MachType MachType { get; private set; }
+    public Status Status { get; private set; }
+    public Location Location { get; private set; }
+    public CreatedAt CreatedAt { get; private set; }
+    public LastUpdatedAt LastUpdatedAt { get; private set; }
+    public Description Description { get; private set; }
+    public MaintenanceStart MaintenanceStart { get; private set; }
+    public MaintenanceReturn MaintenanceReturn { get; private set; }
 
     public void ChangeId(int id) => Id = id;
 
     public Machinery(string name, int machType, string? location, int status, string? description, DateTime? maintenanceStart, DateTime? expectedReturn)
     {
-        MachineryName = new MachineryName(name);
-        MachineryType = new MachineryType(machType);
-        MachineryStatus = new MachineryStatus(status);
-        MachineryLocation = new MachineryLocation(location);
-        MachineryCreatedAt = new MachineryCreatedAt();
-        MachineryLastUpdatedAt = new MachineryLastUpdatedAt();
-        MachineryDescription = new MachineryDescription(description);
-        MaintenanceStartDate = new MaintenanceStartDate(maintenanceStart);
-        ExpectedReturnDate = new ExpectedReturnDate(expectedReturn);
+        Name = new Name(name);
+        MachType = new MachType(machType);
+        Status = new Status(status);
+        Location = new Location(location);
+        CreatedAt = new CreatedAt();
+        LastUpdatedAt = new LastUpdatedAt();
+        Description = new Description(description);
+        MaintenanceStart = new MaintenanceStart(maintenanceStart);
+        MaintenanceReturn = new MaintenanceReturn(expectedReturn);
     }
 
     public Machinery(int id, string name, int machType, int status, string? location, DateTime createdAt, DateTime updatedAt, string? description, DateTime? maintenanceStart, DateTime? expectedReturn) : base(id)
     {
-        MachineryName = new MachineryName(name);
-        MachineryType = new MachineryType(machType);
-        MachineryStatus = new MachineryStatus(status);
-        MachineryLocation = new MachineryLocation(location);
-        MachineryCreatedAt = new MachineryCreatedAt(createdAt);
-        MachineryLastUpdatedAt = new MachineryLastUpdatedAt(updatedAt);
-        MachineryDescription = new MachineryDescription(description);
-        MaintenanceStartDate = new MaintenanceStartDate(maintenanceStart);
-        ExpectedReturnDate = new ExpectedReturnDate(expectedReturn);
+        Name = new Name(name);
+        MachType = new MachType(machType);
+        Status = new Status(status);
+        Location = new Location(location);
+        CreatedAt = new CreatedAt(createdAt);
+        LastUpdatedAt = new LastUpdatedAt(updatedAt);
+        Description = new Description(description);
+        MaintenanceStart = new MaintenanceStart(maintenanceStart);
+        MaintenanceReturn = new MaintenanceReturn(expectedReturn);
     }
 
     private Machinery() { }
