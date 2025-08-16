ea9fff1d0408789bc9f9b3c919789035622d80a9
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-16 13:41:17 -0300

    fix: altera as propriedades que estavam com nomes antigos

--- MachOps.Application/Shared/Raws/MachineryRaw.cs
+++ MachOps.Application/Shared/Raws/MachineryRaw.cs
@@ -1,15 +1,15 @@
 namespace MachOps.Application.Shared.Raws;
 
-public sealed class MachineryRaw(int id, string name, int machType, int status, string? location, DateTime createdAt, DateTime updatedAt, string? description, DateTime? maintenanceStartDate, DateTime? expectedReturnDate)
+public sealed class MachineryRaw(int id, string name, int machType, int status, string? location, DateTime createdAt, DateTime updatedAt, string? description, DateTime? startDate, DateTime? returnDate)
 {
     public int Id { get; } = id;
-    public string MachineryName { get; } = name;
-    public int MachineryType { get; } = machType;
-    public int MachineryStatus { get; } = status;
-    public string? MachineryLocation { get; } = location;
-    public DateTime MachineryCreatedAt { get; } = createdAt;
-    public DateTime MachineryLastUpdatedAt { get; } = updatedAt;
-    public string? MachineryDescription { get; } = description;
-    public DateTime? MaintenanceStartDate { get; } = maintenanceStartDate;
-    public DateTime? ExpectedReturnDate { get; } = expectedReturnDate;
+    public string Name { get; } = name;
+    public int MachType { get; } = machType;
+    public int Status { get; } = status;
+    public string? Location { get; } = location;
+    public DateTime CreatedAt { get; } = createdAt;
+    public DateTime LastUpdatedAt { get; } = updatedAt;
+    public string? Description { get; } = description;
+    public DateTime? Start { get; } = startDate;
+    public DateTime? Return { get; } = returnDate;
 }
